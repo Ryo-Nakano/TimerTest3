be1ee2aa327ba90aa683e0ca3ffc4887ef830210
Create Unity Project <matsuhara9999ryo@yahoo.co.jp>
2018-04-23 15:48:39 +0900

    Initial Commit

---
+++ TimerTest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerTest3
+//
+//  Created by Ryohei Nanano on 2018/04/23.
+//  Copyright © 2018年 GeekSalon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
